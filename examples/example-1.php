5a5d6e7c996cf68f8538aec1eba04c612df89925
Daniel Fekete <daniel.fekete@voov.hu>
2016-02-24 23:35:20 +0100

    extended README

--- examples/example-1.php
+++ examples/example-1.php
@@ -11,8 +11,8 @@ use Billingo\API\DataMapper\BillingoFactory;
 require_once '../vendor/autoload.php';
 
 $client = new Request([
-    'public_key' => '28729a55286ef14dc086224b8678261e',
-    'private_key' => '483c09c3d7e3299db447e3ec48be5a626197c58d590280c59cdd020282a4a957112bb3d9e6e928f75232caa0eae8a531745a0ef2643d4eff3e7b7cb4fa463bab',
+    'public_key' => '',
+    'private_key' => '',
 ]);
 $factory = new BillingoFactory($client);
 //$clients = $factory->make(\Billingo\API\DataMapper\Models\Clients::class)->load('1511187010');
